d400de54cd23d4d227c1253c99519c056bef1466
Artemiy987 <98634526+Artemiy987@users.noreply.github.com>
2022-02-01 16:02:20 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{30,242,2,4,5};
+        bubbleSort.sort(arr);
+        printArray.print(arr);
+        arrayReverse.reverse(arr);
+
+    }
+}
---
+++ src/com/company/printArray.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class printArray {
+    public static void print(int[] arr){
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
